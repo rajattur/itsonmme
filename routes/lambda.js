fd040e5cc03310a74c2e1863f4a534c0adbd3459
rajattur <rajattur@yahoo.com>
2018-03-11 14:49:12 -0500

    added request package. Calling lambda function to get all users
    added request package. Calling lambda function to get all users

--- routes/lambda.js
+++ routes/lambda.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const request = require('request')
+
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.send({
-        message: 'Calling aws.lambda function'
+router.get('/users', function (req, res, next) {
+    request.get('https://mqpzpcke86.execute-api.us-east-1.amazonaws.com/dev/compare-yourself/all',(error, response, body) => {
+        if (error) {
+            return res.send(error)
+        }
+        if (response.statusCode === 200) {
+            res.send(body)
+        }
+        else {
+            res.send({message: 'error from lambda function'})
+        }
+        
+
     })
 });
 
